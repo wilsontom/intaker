5e92df6743638420444b2f19d07b1bf3c1c1d40e
wilsontom <wilsontom89@gmail.com>
2019-10-16 12:03:45 +0100

    import function

--- R/show-method.R
+++ R/show-method.R
@@ -7,7 +7,7 @@ setMethod('show', signature = 'Intake24',
          function(object) {
            cat(cli::rule(
              left = crayon::bold('Intake24 Object'),
-             right = paste0('tidyIntake24 v', packageVersion('tidyIntake24'))
+             right = paste0('tidyIntake24 v', utils::packageVersion('tidyIntake24'))
            ), '\n')
 
            cat(crayon::red('Object Size:', format(utils::object.size(object), units = 'Mb'), '\n', '\n'))
